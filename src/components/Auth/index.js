e03205618f35a6e64ef8f5d9caa60b1cb812464b
Daniel <danielcast244@gmail.com>
2023-08-28 21:11:41 -0500

    ARREGLANDO BOTONES PARA INICIO DE SESION

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,14 +1,17 @@
 import Lottie from 'react-lottie-player'
 import AuthAnimation from '../../../public/assets/animations/auth-page-animation.json'
 import useForm from '@/hooks/useForm'
-import { useContext, useMemo } from 'react'
+import { useContext, useMemo, useState } from 'react'
 import {AiFillFacebook} from 'react-icons/ai';
-import { GlobalContext } from '@/state/context/GlobalContext';
-
+import { GlobalContext, GlobalDispatchContext } from '@/state/context/GlobalContext';
 
 const Auth = () => {
 
-  const state = useContext(GlobalContext)
+  const [isLoginForm, setIsLoginForm] = useState(false)
+
+  const {isAuthenticated, isOnboarded, user, isLoading} = useContext(GlobalContext)
+
+  const dispatch = useContext(GlobalDispatchContext)
 
   const {form, onChangeHandler} = useForm({
     email: '',
@@ -34,7 +37,7 @@ const Auth = () => {
               <div className='my-5 text-5xl tracking-wider'>Instagram</div>
               <input type='email' name='email' id='email' onChange={onChangeHandler} value={form.email} className='w-full px-2 py-1 bg-gray-100 border rounded-sm outline-none hover:bg-transparent focus:bg-transparent placeholder:text-sm focus:border-gray-400' placeholder='Email' />
               <input type='password' name='password' id='password' onChange={onChangeHandler} value={form.password} className='w-full px-2 py-1 transition bg-gray-100 border rounded-sm outline-none hover:bg-transparent focus:bg-transparent placeholder:text-sm focus:border-gray-400' placeholder='Password' />
-              <button type='submit' className='bg-[#0095F6] py-1 text-white active:scale-95 transform transition w-full disabled:bg-opacity-50 disabled:scale-100 rounded text-sm font-semibold' disabled={isDisabled}>Log In</button>
+              <button type='submit' className='bg-[#0095F6] py-1 text-white active:scale-95 transform transition w-full disabled:bg-opacity-50 disabled:scale-100 rounded text-sm font-semibold' disabled={isDisabled}>{isLoginForm ? 'Log In' : 'Sign Up'}</button>
             </form>
               <div className='flex items-center justify-center w-full my-5 space-x-2'>
                 <div className='w-full bg-slate-400 h-[0.8px]' />
@@ -44,12 +47,12 @@ const Auth = () => {
 
               <div className='flex items-center justify-center w-full text-center text-indigo-900'>
                 <AiFillFacebook className='inline-block mr-2 text-2xl' />
-                <span className='text-sm font-semibold'>Log in with Facebook</span>
+                <span className='text-sm font-semibold'>{isLoginForm ? 'Log In' : 'Sign Up'} with Facebook</span>
               </div>
-              <div className='w-full text-xs text-center text-indigo-900'>Forgotten your password?</div>
+              {isLoginForm && <div className='w-full text-xs text-center text-indigo-900'>Forgotten your password?</div>}
           </div>
-          <div className='w-full py-5 space-y-5 text-sm text-center bg-white border border-gray-300'>Don&apos;t have an account? Sign up</div>
-          <span className='ml-2 font-semibold text-indigo-600'>Sign up</span>
+          <div className='w-full py-5 space-y-5 text-sm text-center bg-white border border-gray-300'>{isLoginForm ? 'Don&apos;t have an account?' : 'Already haven an account!'}</div>
+          <span className='ml-2 font-semibold text-indigo-600'>{isLoginForm ? 'Sign Up' : 'Login'}</span>
         </div>
       </div>
     </div>
--- src/state/context/GlobalContext.js
+++ src/state/context/GlobalContext.js
@@ -1,15 +1,15 @@
 import React, { createContext, useReducer } from 'react'
 import { globalReducer } from '../reducers/globalReducer'
 
-export const GlobalContext = createContext(null)
-export const GlobalDispatchContext = createContext(null)
-
 const initialState = {
     user: {},
     isAuthenticated: false,
     isOnboarded: false,
     isLoading: false,
 }
+
+export const GlobalContext = createContext(initialState)
+export const GlobalDispatchContext = createContext(null)
 
 const GlobalContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(globalReducer, initialState)
