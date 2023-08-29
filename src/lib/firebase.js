00908f3343592fab268e4780c1120f60a772589c
Daniel <danielcast244@gmail.com>
2023-08-28 19:16:58 -0500

    SE CREO EL GLOBAL CONTEXT Y SE CONFIGURO EL FIREBASE EN EL JS

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,12 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApps, initializeApp, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {getAuth} from "firebase/auth"
+import {getStorage} from "firebase/storage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    
+
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore()
+const auth = getAuth()
+const storage = getStorage()
+
+export {app, db, auth, storage}
